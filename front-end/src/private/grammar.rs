75780b0b6dbb99958ecfa3849dcd7ee18d55d3c8
JCD09 <mishachavarha@gmail.com>
2023-12-11 13:32:28 +0200

    clean

--- front-end/src/private/grammar.rs
+++
@@ -1,7 +0,0 @@
-/// This file is just for mini java grammar definition. 
-/// 
-/// 
-
-pub(crate) const GRAMMAR: &str = "
-
-";
--- scanner/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-mod parser;
-
-#[cfg(test)]
-mod tests {
-}
